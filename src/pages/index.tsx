741cdbaa36a8cd8bae90e268a3b8b19a2163ba10
yuka-sawamura <sawasun1016@gmail.com>
2025-04-01 19:27:47 +0900

    訪問理美容　ittan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -359,6 +359,29 @@ export default function Home() {
               </div>
             </div>
           </section>
+
+          <section>
+            <div className="itit_visit-beauty">
+              <div className="itit_visit-beauty_group">
+                <div>
+                  <img
+                    className="itit_visit-beauty_image"
+                    src="/visit-beauty.png"
+                    alt="訪問理美容の画像"
+                  />
+                </div>
+                <div className="itit_visit-beauty_text-group">
+                  <h2 className="itit_visit-beauty_title">訪問理美容</h2>
+                  <p className="itit_visit-beauty_text">
+                    介護・医療施設への訪問美容も実施しています。
+                  </p>
+                  <p className="itit_visit-beauty_text">
+                    お気軽にお問い合わせください。
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
         </main>
       </div>
     </>
