6a63ca65a8a293cc25a836cfffa9c1f447d485d0
yuka-sawamura <sawasun1016@gmail.com>
2025-05-29 15:02:37 +0900

    脱毛　pc ittan

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,32 +4,34 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const hundleMenuOpen = () => setIsOpen(false);
   return (
-    <header className="itit_header">
-      <div className="itit_header_group">
-        <div className="itit_header_text">
-          <div className="itit_header_title">it feels it</div>
-          <ul className="itit_header_p-group">
-            <li className="itit_header_p">
-              <a href="#menu">メニュー</a>
-            </li>
-            <li className="itit_header_p">
-              <a href="#staff">スタッフ</a>
-            </li>
-            <li className="itit_header_p">
-              <a href="#information">新着情報</a>
-            </li>
-            <li className="itit_header_p">
-              <a href="#access">アクセス</a>
-            </li>
-          </ul>
+    <section id="/beauty-salon">
+      <header className="itit_header">
+        <div className="itit_header_group">
+          <div className="itit_header_text">
+            <div className="itit_header_title">it feels it</div>
+            <ul className="itit_header_p-group">
+              <li className="itit_header_p">
+                <a href="#menu">メニュー</a>
+              </li>
+              <li className="itit_header_p">
+                <a href="#staff">スタッフ</a>
+              </li>
+              <li className="itit_header_p">
+                <a href="#information">新着情報</a>
+              </li>
+              <li className="itit_header_p">
+                <a href="#access">アクセス</a>
+              </li>
+            </ul>
+          </div>
+          <div className="itit_header_pages-group">
+            <a className="itit_header_pages" href="/removal" target="_blank">
+              脱毛ページ
+            </a>
+          </div>
         </div>
-        <div className="itit_header_pages-group">
-          <a className="itit_header_pages" href="#">
-            脱毛ページ
-          </a>
-        </div>
-      </div>
-    </header>
+      </header>
+    </section>
   );
 };
 export default Header;
