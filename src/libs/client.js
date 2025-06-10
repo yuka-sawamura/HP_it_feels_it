4a889bf7cea7ab789b852811742e769251ac47d8
yuka-sawamura <sawasun1016@gmail.com>
2025-06-10 21:30:30 +0900

    env key 　変更

--- src/libs/client.js
+++ src/libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: "it-feels-it",
-  apiKey: process.env.MICROCMS_KEY,
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_KEY,
 });
